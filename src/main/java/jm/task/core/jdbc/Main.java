617c4cac8ed9ec5d446efb029d50cae91d520c92
Atecc <rizvan1127@gmail.com>
2023-10-19 14:52:25 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
+public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
+    public static void main(String[] args) {
+        userService.createUsersTable();
+
+        userService.saveUser("Boris", "Britva", (byte) 21);
+        userService.saveUser("Alex", "Petrov", (byte) 21);
+        userService.saveUser("Ivan", "Ivanov", (byte) 21);
+        userService.saveUser("Ivan", "Petrov", (byte) 21);
+
+        userService.removeUserById(3);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
