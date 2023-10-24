805a7a593d47c156d3c8592f33d5d15cb27f81c2
Atecc <rizvan1127@gmail.com>
2023-10-24 20:19:10 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,9 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.model.User;
 
-import java.util.List;
 
 public class Main {
     private final static UserService userService = new UserServiceImpl();
@@ -18,9 +16,7 @@ public class Main {
         userService.saveUser("Ivan", "Petrov", (byte) 21);
 
         userService.removeUserById(3);
-        for (User user1 :userService.getAllUsers() ) {
-            System.out.println(user1);
-        }
+        userService.getAllUsers().forEach(System.out::println);
 
 
         userService.cleanUsersTable();
